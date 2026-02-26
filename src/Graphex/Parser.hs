d54698d0de52b21eb45070ee82ec52a813129697
parsonsmatt <parsonsmatt@gmail.com>
2026-02-25 17:45:57 -0700

    Speed up parsing

--- src/Graphex/Parser.hs
+++ src/Graphex/Parser.hs
@@ -13,29 +13,27 @@
 -- graphex, this makes sense in a way - those are all still dependencies.
 module Graphex.Parser where
 
-import           Data.Either          (rights)
+import           Data.Maybe           (mapMaybe)
 import           Data.String          (IsString)
 import           Data.Text            (Text)
 import qualified Data.Text            as T
-import qualified Data.Text.IO         as TIO
+import qualified Data.Text.Lazy       as TL
+import qualified Data.Text.Lazy.IO    as TLIO
 import           Data.Void
+import           System.IO            (withFile, IOMode(ReadMode))
 
-import           Replace.Megaparsec   (sepCap, streamEdit)
 import           Text.Megaparsec
 import           Text.Megaparsec.Char
 
 import           Graphex.Core
 
+-- | Parse a single import statement from a line known to start with "import".
 importParser
   :: MonadParsec e s m
   => Token s ~ Char
   => IsString (Tokens s)
   => m Import
 importParser = do
-  -- This newline helps us avoid the word "import" in comments or identifiers.
-  -- Basically, we insist imports are at the beginning of a line (which Haskell does
-  -- too I think)
-  _ <- newline
   _ <- string "import"
   space1
   -- Handle prefix qualified imports
@@ -52,30 +50,57 @@ importParser = do
     , package = T.pack <$> pkg
     }
 
-importsParser
-  :: forall e s m
-   . MonadParsec e s m
-  => Token s ~ Char
-  => IsString (Tokens s)
-  => m [Import]
-importsParser = rights <$> sepCap importParser
+-- | Try to parse a strict Text line as an import.
+parseImportLine :: Text -> Maybe Import
+parseImportLine line =
+  case runParser (importParser @Void @Text) "" line of
+    Left _  -> Nothing
+    Right x -> Just x
 
-removeBlockComments :: Text -> Text
-removeBlockComments = streamEdit (blockCommentParser @Void) (\_ -> "")
+-- | Extract imports from lazy text using line-based scanning.
+--
+-- Splits into lines, strips block comments, takes only the file header
+-- (up to the first line of code), then parses the import lines.
+-- With lazy IO, only the top of the file is read from disk.
+extractImports :: TL.Text -> [Import]
+extractImports = mapMaybe (parseImportLine . TL.toStrict)
+               . filter isImportLine
+               . takeWhile isHeaderLine
+               . stripBlockComments
+               . TL.lines
 
-blockCommentParser
-  :: forall e s m
-   . MonadParsec e s m
-  => Token s ~ Char
-  => IsString (Tokens s)
-  => m String
-blockCommentParser = do
-  _ <- string "{-"
-  manyTill anySingle (string "-}")
+-- | Remove lines that fall inside block comments.
+stripBlockComments :: [TL.Text] -> [TL.Text]
+stripBlockComments = go False
+    where
+        go _ [] = []
+        go True (l:ls)
+            | TL.isInfixOf "-}" l = go False ls
+            | otherwise           = go True ls
+        go False (l:ls)
+            | isOpenComment l = go (not $ TL.isInfixOf "-}" l) ls
+            | otherwise       = l : go False ls
+
+        isOpenComment l = let s = TL.stripStart l
+                          in TL.isPrefixOf "{-" s && not (TL.isPrefixOf "{-#" s)
+
+-- | Lines that can appear in the file header before code begins.
+isHeaderLine :: TL.Text -> Bool
+isHeaderLine l =
+    isImportLine l
+    || TL.null (TL.strip l)
+    || TL.isPrefixOf "--"     s
+    || TL.isPrefixOf "{-#"    s
+    || TL.isPrefixOf "module " l
+    || TL.isPrefixOf "#"      s
+    where s = TL.stripStart l
+
+isImportLine :: TL.Text -> Bool
+isImportLine = TL.isPrefixOf "import "
 
 parseFileImports :: FilePath -> IO [Import]
-parseFileImports fp = do
-  contents <- removeBlockComments <$> TIO.readFile fp
-  case runParser (importsParser @Void) fp contents of
-    Left err -> error $ unlines $ [mconcat ["Failed to parse ", fp, ":"], errorBundlePretty err]
-    Right x -> pure x
+parseFileImports fp =
+  withFile fp ReadMode $ \h -> do
+    contents <- TLIO.hGetContents h
+    let !imports = extractImports contents
+    pure imports
